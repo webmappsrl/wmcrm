51a225943edaf7af76d98d658c2355b55b040f6d
Alessio Piccioli <piccioli@netseven.it>
2023-01-31 15:38:49 +0100

    Model Milestone created

---
+++ database/migrations/2023_01_31_142726_create_milestones_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMilestonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('milestones', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            // nome, descrizione, scadenza
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->date('expire')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('milestones');
+    }
+}
