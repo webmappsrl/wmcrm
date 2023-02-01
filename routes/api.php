de7af0cf8dd10a4b100e81026eb2b31b86be4752
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-02-01 15:44:37 +0100

    Created Milestone API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Resources\MilestoneResource;
+use App\Models\Milestone;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('milestone', function () {
+    $milestone = App\Models\Milestone::all();
+    return response()->json($milestone);
 });
